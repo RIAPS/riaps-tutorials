9c7167bedb69ed0772324c940c819c0ef641b38d
timkrentz <tskrentz@gmail.com>
2023-05-11 12:56:03 -0500

    Update files

---
+++ LearnPubSub/Listener.py
@@ -0,0 +1,9 @@
+from riaps.run.comp import Component
+import spdlog
+
+
+
+class Listener(Component):
+
+    def __init__(self):
+        super(Listener, self).__init__()
---
+++ LearnTimer/Ticker.py
@@ -0,0 +1,9 @@
+from riaps.run.comp import Component
+import spdlog
+
+
+class Ticker(Component):
+
+    def __init__(self):
+        super(Ticker, self).__init__()
+
