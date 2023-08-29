451b231a593b1080041b556c0534e3747da4fdc5
timkrentz <tskrentz@gmail.com>
2023-08-29 11:46:45 -0500

    Update PubSub naming

--- PubSub/Bar.py
+++ PubSub/Bar.py
@@ -1,9 +1,9 @@
 from riaps.run.comp import Component
 
-class Listener(Component):
+class Bar(Component):
     def __init__(self):
-        super(Listener, self).__init__()
+        super(Bar, self).__init__()
         
     def on_subPort(self):
         msg = self.subPort.recv_pyobj()
-        self.logger.info(f"Listener received: {msg}")
+        self.logger.info(f"Bar received: {msg}")
--- PubSub/Foo.py
+++ PubSub/Foo.py
@@ -1,13 +1,13 @@
 from riaps.run.comp import Component
 
-class Talker(Component):
+class Foo(Component):
 
     def __init__(self):
-        super(Talker, self).__init__()
+        super(Foo, self).__init__()
         self.counter = 0
 
     def on_ticker(self):
         msg = self.ticker.recv_pyobj()
-        self.logger.info(f"Talker sends: {self.counter}")
+        self.logger.info(f"Foo sends: {self.counter}")
         self.pubPort.send_pyobj(self.counter)
         self.counter += 1
